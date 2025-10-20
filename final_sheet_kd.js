47f617628bffe8ef429d78c102011b32644d7c8e
Jahn <vieirajahnlucas@gmail.com>
2025-10-20 18:03:19 -0300

    Main Screen Complete, 2nd started
    now you can send all the character info (except image) and name and class will be shown in another page, when trying to submit with some of the infos empty an error message will appear

---
+++ final_sheet_kd.js
@@ -0,0 +1,2 @@
+document.getElementById("char_namespace").textContent = sessionStorage.getItem("char_namespace")
+document.getElementById("Class_namespace").textContent = sessionStorage.getItem("Class_namespace")
---
+++ settin_kd.js
@@ -0,0 +1,159 @@
+
+// #######################################  Stat-Sheet Handling  ####################################### //
+
+//creates a class for the stats
+class Stat{
+    constructor(stat_name, level, bonus) {
+        this.stat_name = stat_name
+        this.level = level
+        this.bonus = bonus
+    }
+}
+
+//sets the base stats as new objects
+var strength = new Stat("strength",10,0);
+var dexterity = new Stat("dexterity",10,0);
+var constitution = new Stat("constitution",10,0);
+var inteligence = new Stat("inteligence",10,0);
+var wisdom = new Stat("wisdom",10,0);
+var charisma = new Stat("charisma",10,0);
+
+//stat array
+let stats = [strength, dexterity, constitution, inteligence, wisdom, charisma]
+//points to change the stats
+var skill_points = 9
+
+
+// // // // js --> html funções
+const stats_sheet = document.getElementById("stats_sheet"); //gets the stat sheet div from html
+
+//circles from every stat on the list and creates a div for it
+stats.forEach(stat => {
+    const stt_n = document.createElement("h2"); //name of the stat
+    stt_n.textContent = stat.stat_name;
+
+    const up_stt = document.createElement("button"); //up stat level button
+    up_stt.textContent = "+";
+    up_stt.type = "button";
+
+    const stt_lvl = document.createElement("h1"); //stat level
+    stt_lvl.textContent = stat.level;
+
+    const down_stt = document.createElement("button"); //down stat level button
+    down_stt.textContent = "-";
+    down_stt.type = "button";
+
+    const stt_bon = document.createElement("h2"); //text with the bonus of that the level gives
+    stt_bon.textContent = "+" + stat.bonus;
+
+    const stt_div = document.createElement("div"); //creates a div to hold these items
+    stt_div.className = "stat_container";
+
+    //adds the items to the div
+    stt_div.appendChild(stt_n);
+    stt_div.appendChild(down_stt);
+    stt_div.appendChild(stt_lvl);
+    stt_div.appendChild(up_stt);
+    stt_div.appendChild(stt_bon);
+
+    //put the div in the sheet
+    stats_sheet.appendChild(stt_div);
+
+    stat.stt_lvl = stt_lvl;
+    stat.stt_bon = stt_bon;
+
+    //updates de bonus of the stat based on the player level
+    function update_bonus_value() {
+        if(stat.level > 10) {
+            stat.bonus = Math.floor((stat.level - 10) / 2); //adds +1 for every even number above 10
+            stt_bon.textContent = "+" + stat.bonus;
+        }
+        
+        if(stat.level < 10) {
+            stat.bonus = ((9 - stat.level) / 2) + 1; //adds -1 for every odd number below 10
+            stat.bonus = Math.floor(stat.bonus)
+            stt_bon.textContent = "-" + stat.bonus;
+        }
+
+        if(stat.level === 10) { //if the stat is 10 the bonus is 0
+            stat.bonus = 0;
+            stt_bon.textContent = "+" + stat.bonus;
+        }
+        
+    }
+
+    function update_skill_points() {
+        document.getElementById("skill_points").textContent = skill_points
+    }
+
+    update_skill_points()
+
+    up_stt.addEventListener("click", () => { //function to increase the level on the button press
+        if(skill_points > 0){ //only works if there's points for it
+            stat.level++;
+            stt_lvl.textContent = stat.level;
+
+            skill_points -= 1} //take out points
+
+            update_bonus_value() //calls the update bonus function
+            update_skill_points()
+        });
+
+    down_stt.addEventListener("click", () => { //function to decrease the level on the button press
+        if (stat.level > 0) { //only works if the level is above 0, to prevent negative numbers
+        stat.level--;
+        stt_lvl.textContent = stat.level;
+        skill_points += 1} //adds points
+
+        update_bonus_value() //calls the update bonus function
+        update_skill_points()
+    })
+    
+})
+
+// // // reset button
+const reset_sheet_btn = document.createElement("button"); //creates the reset button
+reset_sheet_btn.textContent = "reset"; //button text
+reset_sheet_btn.type = "button"; //prevents the entire page from reloading
+
+reset_sheet_btn.addEventListener("click", () => {
+    stats.forEach(stat => { //circles every stat and resets it's level and bonus
+        stat.level = 10;
+        //stat.bonus = 0;
+        update_bonus_value()
+        update_skill_points()
+
+        stat.stt_lvl.textContent = stat.level;
+        stat.stt_bon.textContent = "+" + 0;
+    })
+    skill_points = 9; //reset the points
+})
+
+stats_sheet.appendChild(reset_sheet_btn); //inserts the button on the stat sheet
+
+// #######################################  Page Transition Handler  ####################################### //
+
+function next_page() {
+    const all_stats = [
+        //{id:"char_img"},
+        {id:"char_namespace"},
+        {id:"Class_namespace"},
+        {id:"race_namespace"},
+        {id:"player_name_namespace"},
+        {id:"alingment_namespace"},
+        {id:"background_namespace"}
+    ];
+
+    all_stats.forEach(stat => {
+        let val = document.getElementById(stat.id).value.trim();
+        if (!val) {
+            document.getElementById("error_msg").textContent = "Not all fields were filled, please check again!";
+            return;
+        }
+        else {
+            sessionStorage.setItem(stat.id, val)
+        }
+    })
+
+    window.location.href = "http://127.0.0.1:5500/D&D_char_sheet/final_sheet.html";
+}
